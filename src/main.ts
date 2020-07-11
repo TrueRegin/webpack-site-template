a8192cb6e83f6034225d8f69956ecfe9607fe945
MoCreatureGuy <36866793+MoCreatureGuy@users.noreply.github.com>
2020-07-11 13:01:13 -0400

    Added more info to main page, made it easier to remove the animation at the beginning, added font-awesome, mini-css-extract-plugin, and some other webpack loaders. Also made it possible for webpack to read fonts and normal .css files. Should now be much easier to use with projects out of the box.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
 import '@/scss/styles.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+/** Disable or remove the below line to get a blank slate to start with */
+import '@/scss/demo.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 /** @type {import("webpack").WebpackOptions} */
 
@@ -25,13 +26,20 @@ const config = {
             filename: 'index.html',
             template: path.join(__dirname, './src/pages/index.pug'),
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
     ],
     // ? What loaders to apply to what files
     module: {
         rules: [
             {
-                test: /\.scss$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+                test: /\.svg/,
+                use: {
+                    loader: 'svg-url-loader',
+                    options: {},
+                },
             },
             {
                 test: /\.ts$/,
@@ -40,6 +48,10 @@ const config = {
             {
                 test: /\.pug$/,
                 loaders: ['pug-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|ttf|eot|woff(2)?)$/i,
+                loaders: ['file-loader'],
             },
         ],
     },
@@ -54,9 +66,29 @@ const config = {
 module.exports = (env, argv) => {
     if (argv.mode === 'development') {
         // ? Override the config object with production only options
+        config.module.rules.push(
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader'],
+            }
+        );
     }
     if (argv.mode === 'production') {
         // ? Override the config object with production only options
+        config.module.rules.push(
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+        );
     }
 
     return config;
